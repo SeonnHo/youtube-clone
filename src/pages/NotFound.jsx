aa5244523d0436810c778a86369f2deab1930ff5
SeonnHo <prefer914@gmail.com>
2024-02-18 23:58:06 +0900

    not found 페이지 UI 스타일 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <div>Not Found 😅</div>;
+  return (
+    <main className="flex justify-center items-center h-screen w-screen">
+      <p className="font-bold text-5xl">Not Found 😅</p>
+    </main>
+  );
 }
