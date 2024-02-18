d9860d8264def9468356984e181b1462f17229e4
SeonnHo <prefer914@gmail.com>
2024-02-18 23:56:19 +0900

    root 페이지 삭제 후 해당 코드 app으로 이동 및 router 처리 코드 index로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.css';
-import Root from './pages/Root';
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
+import SearchHeader from './components/SearchHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { YoutubeApiProvider } from './context/YoutubeApiContext';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
-]);
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <SearchHeader />
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
+    </>
+  );
 }
 
 export default App;
