8ba3c88ea4331a9121deffb458eda9f658e66c11
SeonnHo <prefer914@gmail.com>
2024-02-21 21:30:39 +0900

    QueryClient 인스턴스 refetchOnWindowFocus 속성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { YoutubeApiProvider } from './context/YoutubeApiContext';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
