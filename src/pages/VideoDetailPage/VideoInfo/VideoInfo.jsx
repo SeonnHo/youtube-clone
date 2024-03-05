e4532236635939ce3778b2ae6a716a2c6aeae74d
SeonnHo <prefer914@gmail.com>
2024-03-05 23:44:06 +0900

    break point 변경 및 설명 길이 체크 로직 변경

--- src/pages/VideoDetailPage/VideoInfo/VideoInfo.jsx
+++ src/pages/VideoDetailPage/VideoInfo/VideoInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { compactNumberFormatter, dateFormatter } from '../../../utility/format';
 
 export default function VideoInfo({ video, channel }) {
@@ -6,9 +6,6 @@ export default function VideoInfo({ video, channel }) {
   const { viewCount } = video.statistics;
   const { subscriberCount } = channel.statistics;
   const { thumbnails } = channel.snippet;
-
-  const ref = useRef(null);
-
   const [isOverflow, setIsOverflow] = useState(false);
   const [isShowMore, setIsShowMore] = useState(false);
 
@@ -16,15 +13,16 @@ export default function VideoInfo({ video, channel }) {
     setIsShowMore((prev) => !prev);
   };
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (ref.current.offsetHeight > 160) {
-        setIsOverflow(true);
-      } else {
-        setIsOverflow(false);
-      }
-    }, 100);
+  const isOverflowCheck = (text) => {
+    if (text?.split('\n').length > 4) {
+      setIsOverflow(true);
+    } else {
+      setIsOverflow(false);
+    }
+  };
 
+  useEffect(() => {
+    isOverflowCheck(video.snippet.description);
     return () => {
       setIsOverflow(false);
       setIsShowMore(false);
@@ -33,8 +31,8 @@ export default function VideoInfo({ video, channel }) {
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="w-full text-xl font-bold mt-4 max-sm:px-2">{title}</h2>
-      <div className="w-full flex flex-row my-3 max-sm:px-2">
+      <h2 className="w-full text-xl font-bold mt-4 max-lg:px-2">{title}</h2>
+      <div className="w-full flex flex-row my-3 max-lg:px-2">
         <img
           className="w-12 h-12 rounded-full mr-3"
           src={thumbnails.default.url}
@@ -48,17 +46,16 @@ export default function VideoInfo({ video, channel }) {
         </div>
       </div>
       <div
-        className={`w-full max-sm:w-[calc(100%-1rem)] flex flex-col bg-zinc-800 rounded-2xl p-4 ${
+        className={`w-full max-lg:w-[calc(100%-1rem)] flex flex-col bg-zinc-800 rounded-2xl p-4 ${
           isOverflow ? (isShowMore ? '' : 'max-h-[160px]') : ''
         }`}
-        ref={ref}
       >
         <p className="text-sm font-bold">
           조회수 {compactNumberFormatter.format(viewCount)}회{' '}
           {dateFormatter(new Date(publishedAt))}
         </p>
         <p
-          className={`text-zinc-300 text-sm font-bold whitespace-pre-wrap ${
+          className={`text-zinc-300 text-sm font-bold whitespace-pre-line break-words ${
             isOverflow ? (isShowMore ? 'mb-4' : 'line-clamp-4') : ''
           }`}
         >
