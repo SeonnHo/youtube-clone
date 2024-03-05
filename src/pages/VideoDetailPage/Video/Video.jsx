be52555357cbfb4b1a875dbd6a3b5252a0b65036
SeonnHo <prefer914@gmail.com>
2024-03-05 23:34:20 +0900

    Video 컴포넌트 break point 변경

--- src/pages/VideoDetailPage/Video/Video.jsx
+++ src/pages/VideoDetailPage/Video/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Video({ video }) {
   return (
     <iframe
-      className="w-full h-full rounded-2xl max-sm:rounded-none"
+      className="w-full h-full rounded-2xl max-lg:rounded-none"
       src={`https://www.youtube.com/embed/${video.id}?autoplay=1&modestbranding=1`}
       title="Youtube video player"
       allowFullScreen
