5cbddf157e7856032b02ce343310d5c5ef454ad1
SeonnHo <prefer914@gmail.com>
2024-03-06 19:34:42 +0900

    useWindowSize를 활용한 반응형으로 변경

--- src/pages/SearchVideosPage/SearchVideosPage.jsx
+++ src/pages/SearchVideosPage/SearchVideosPage.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useYoutubeApi } from '../../context/YoutubeApiContext';
 import { compactNumberFormatter, dateFormatter } from '../../utility/format';
-import {
-  replaceHtmlEntities,
-  replaceImageWithMaxRes,
-} from '../../utility/replace';
+import { replaceHtmlEntities } from '../../utility/replace';
 import {
   extractChannelIdList,
   extractVideoIdList,
 } from '../../utility/extract';
+import VideoThumbnail from '../../components/VideoThumbnail';
+import ChannelThumbnail from '../../components/ChannelThumbnail';
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function SearchVideosPage() {
   const { keyword } = useParams();
@@ -48,6 +48,7 @@ export default function SearchVideosPage() {
   });
 
   const navigate = useNavigate();
+  const windowSize = useWindowSize();
 
   const handleClick = (id, video) => {
     let resultChannel;
@@ -69,84 +70,107 @@ export default function SearchVideosPage() {
     return <p>Something is wrong!</p>;
 
   return (
-    <main className="flex flex-col w-full">
+    <main className="flex flex-col w-full sm:max-lg:px-2">
       {videoDetails.map((video) => {
-        return (
-          <section
-            key={video.id}
-            className="flex max-sm:flex-col mb-4 cursor-pointer"
-            onClick={() => handleClick(video.id, video)}
-          >
-            <img
-              className={`shrink-0 rounded-xl max-sm:hidden`}
-              src={replaceImageWithMaxRes(video.snippet.thumbnails.medium.url)}
-              alt="thumbnail"
-              width={video.snippet.thumbnails.medium.width}
-              height={video.snippet.thumbnails.medium.height}
-            />
-            <img
-              className={`shrink-0 w-full sm:hidden`}
-              src={replaceImageWithMaxRes(video.snippet.thumbnails.medium.url)}
-              alt="thumbnail"
-            />
+        if (windowSize.width > 640) {
+          // 브라우저 width가 640px 보다 클 때 렌더링
+          return (
+            <section
+              key={video.id}
+              className="flex mb-4 cursor-pointer"
+              onClick={() => handleClick(video.id, video)}
+            >
+              <VideoThumbnail
+                className={`shrink-0 rounded-xl w-[${video.snippet.thumbnails.medium.width}px] h-[${video.snippet.thumbnails.medium.height}px]`}
+                video={video}
+                maxres={false}
+              />
 
-            <div className="ml-4 max-sm:m-2">
-              <div className="flex sm:flex-col">
-                {channels.map((channel) => {
-                  if (channel.id === video.snippet.channelId) {
-                    return (
-                      <img
-                        key={channel.id}
-                        className="w-8 h-8 my-1 mx-2 rounded-full sm:hidden"
-                        src={channel.snippet.thumbnails.default.url}
-                        alt="channel thumbnails"
-                      />
-                    );
-                  }
-                  return null;
-                })}
-                <div className="max-sm:flex max-sm:flex-col">
-                  <h2 className="max-sm:text-sm font-bold line-clamp-1 max-sm:line-clamp-2">
+              <div className="ml-4">
+                <div className="flex flex-col">
+                  <h2 className="font-bold line-clamp-1">
                     {replaceHtmlEntities(video.snippet.title)}
                   </h2>
-                  <div className="max-sm:flex">
-                    <p className="text-sm max-sm:text-xs text-zinc-300 line-clamp-1 sm:hidden">
-                      {video.snippet.channelTitle}&nbsp;•&nbsp;
-                    </p>
-                    <p className="text-sm max-sm:text-xs text-zinc-300 line-clamp-1">
-                      조회수{' '}
-                      {compactNumberFormatter.format(
-                        video.statistics.viewCount
-                      )}
-                      회 • {dateFormatter(new Date(video.snippet.publishedAt))}
-                    </p>
-                  </div>
+                  <p className="text-sm text-zinc-300 line-clamp-1">
+                    조회수{' '}
+                    {compactNumberFormatter.format(video.statistics.viewCount)}
+                    회 • {dateFormatter(new Date(video.snippet.publishedAt))}
+                  </p>
                   <div className="flex items-center">
                     {channels.map((channel) => {
                       if (channel.id === video.snippet.channelId) {
                         return (
-                          <img
+                          <ChannelThumbnail
                             key={channel.id}
-                            className="w-7 h-7 m-2 rounded-full max-sm:hidden"
-                            src={channel.snippet.thumbnails.default.url}
-                            alt="channel thumbnails"
+                            className={'w-7 h-7 m-2 rounded-full'}
+                            channel={channel}
                           />
                         );
                       }
                       return null;
                     })}
-                    <p className="text-sm max-sm:text-xs text-zinc-300 line-clamp-1 max-sm:hidden">
+                    <p className="text-sm text-zinc-300 line-clamp-1">
                       {video.snippet.channelTitle}
                     </p>
                   </div>
                 </div>
+                <p className="text-sm text-zinc-300 line-clamp-1">
+                  {video.snippet.description}
+                </p>
               </div>
-              <p className="text-sm max-sm:text-xs text-zinc-300 line-clamp-1 max-sm:hidden">
-                {video.snippet.description}
-              </p>
-            </div>
-          </section>
-        );
+            </section>
+          );
+        } else {
+          // 브라우저가 width가 640px보다 작을 때 렌더링
+          return (
+            <section
+              key={video.id}
+              className="flex flex-col mb-4 cursor-pointer"
+              onClick={() => handleClick(video.id, video)}
+            >
+              <VideoThumbnail
+                className={'shrink-0 w-full'}
+                video={video}
+                maxres={true}
+              />
+
+              <div className="m-2">
+                <div className="flex">
+                  {channels.map((channel) => {
+                    if (channel.id === video.snippet.channelId) {
+                      return (
+                        <ChannelThumbnail
+                          key={channel.id}
+                          className={'w-8 h-8 my-1 mx-2 rounded-full'}
+                          channel={channel}
+                        />
+                      );
+                    }
+                    return null;
+                  })}
+                  <div className="flex flex-col">
+                    <h2 className="text-sm font-bold line-clamp-2">
+                      {replaceHtmlEntities(video.snippet.title)}
+                    </h2>
+                    <div className="flex">
+                      <p className="text-xs text-zinc-300 line-clamp-1">
+                        {video.snippet.channelTitle}&nbsp;•&nbsp;
+                      </p>
+                      <p className="text-xs text-zinc-300 line-clamp-1">
+                        조회수{' '}
+                        {compactNumberFormatter.format(
+                          video.statistics.viewCount
+                        )}
+                        회 •{' '}
+                        {dateFormatter(new Date(video.snippet.publishedAt))}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          );
+        }
       })}
     </main>
   );
